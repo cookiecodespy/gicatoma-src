fad3c55aebba84e6d78ca7149affad48f42e31a9
Tomás Sotz <tomas.sotz@outlook.com>
2026-05-06 03:16:30 -0400

    Corrige base path para publicar en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repositoryBase = '/gicatoma-src/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: repositoryBase,
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
